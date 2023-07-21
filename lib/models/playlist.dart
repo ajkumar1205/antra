bf9938e77ec093549e10342ed609edd301d77e9c
thakurajay369 <ajthakur1205@gmail.com>
2023-07-21 19:44:25 +0530

    Merged both branches

---
+++ lib/models/playlist.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+
+@HiveType(typeId: 0)
+class PlayList extends HiveObject {
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  String? description;
+
+  @HiveField(2)
+  String? artist;
+
+  @HiveField(3)
+  bool oneArtist = false;
+
+  @HiveField(4)
+  ConcatenatingAudioSource? songs;
+
+  PlayList({
+    required this.title,
+    required this.songs,
+    this.description,
+    required this.oneArtist,
+    this.artist,
+  });
+
+  PlayList.fromList({
+    required this.title,
+    required this.description,
+    required this.oneArtist,
+    this.artist,
+    required List<SongModel> list,
+  }) {
+    list.forEach(
+      (song) {
+        songs!.children.add(
+          AudioSource.uri(
+            Uri.parse(song.data),
+            tag: MediaItem(
+              id: song.id.toString(),
+              title: song.title,
+              artist: song.artist,
+              extras: {
+                'artWork': QueryArtworkWidget(
+                  id: song.id,
+                  type: ArtworkType.AUDIO,
+                  nullArtworkWidget: const Icon(Icons.music_note),
+                )
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/provider/songlist_provider.dart
+++ lib/provider/songlist_provider.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 
 class SongList extends ChangeNotifier {
   List<SongModel>? songs;
   var hasSongs = false;
   OnAudioQuery audioQuery = OnAudioQuery();
+  ConcatenatingAudioSource? list;
 
   Future<void> takePermission() async {
     var status = await audioQuery.permissionsStatus();
@@ -19,5 +22,31 @@ class SongList extends ChangeNotifier {
     songs = await audioQuery.querySongs();
     hasSongs = true;
     notifyListeners();
+  }
+
+  ConcatenatingAudioSource? get shuffleableList {
+    if (list == null && !hasSongs) return null;
+    if (list == null) {
+      songs!.forEach((song) {
+        list!.add(
+          AudioSource.uri(
+            Uri.parse(song.data),
+            tag: MediaItem(
+              id: song.id.toString(),
+              title: song.title,
+              artist: song.artist,
+              extras: {
+                'artWork': QueryArtworkWidget(
+                  id: song.id,
+                  type: ArtworkType.AUDIO,
+                  nullArtworkWidget: const Icon(Icons.music_note),
+                )
+              },
+            ),
+          ),
+        );
+      });
+    }
+    return list;
   }
 }
